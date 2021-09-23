e2b3f82cc34339b81b2814632df6e7113bd93d18
Ben Mitchinson <mitchinson.dev@gmail.com>
2021-09-23 01:56:52 -0500

    Send inrix every 2 minutes to connected client

--- src/ingest_inrix.ts
+++ src/ingest_inrix.ts
@@ -2,6 +2,9 @@
 File to mock urban os
 Ingest inrix dataset with hardcoded secrets, provide it to a websocket for
 index.ts to utilize. index.ts doesn't know it's not coming from urban os
+
+This must be started before starting index.ts, and restarted if client 
+disconnects and connects again
 */
 
 /* 
@@ -31,32 +34,55 @@ import fetch from 'node-fetch';
 import { WebSocketServer } from 'ws';
 
 const tokenURL = `http://na.api.inrix.com/Traffic/Inrix.ashx?Action=GetSecurityToken&Vendorid=${process.env.VENDORID}&Consumerid=${process.env.CONSUMERID}&format=json`;
+const inrixURL =
+    'http://na.api.INRIX.com/traffic/Inrix.ashx?Action=GetSegmentSpeedInBox&Corner1=40.1255678864668|-82.76221584235948&Corner2=39.81278590756092|-83.24321102058161&Duration=0&Resolution=0&RoadSegmentType=TMC,XDS&Units=0&format=json&token=';
 
 let token = '';
+let client = undefined as any;
 
-const getToken = async () => {
+const refreshToken = async () => {
     const r = await fetch(tokenURL);
     const j = await r.json();
-    return j.result.token;
+    token = j.result.token;
+    log('New token set');
+};
+
+const fetchInrixData = async () => {
+    const r = await fetch(inrixURL + token);
+    const j = await r.json();
+    const time = j.result?.segmentSpeeds[0]?.timestamp;
+    return j.result?.segmentSpeeds[0]?.segments.map((seg: any) => {
+        return { payload: { ...seg, ingestion: time } };
+    });
 };
 
 const main = async () => {
     log('Starting up the mock urban os!');
 
     // Get a new token to use for requesting inrix data every 30 minutes
-    setInterval(async () => {
-        token = await getToken();
-        log('New token set');
-        // }, 30 * 60000);
-    }, 10000);
+    await refreshToken();
+    setInterval(refreshToken, 30 * 60000); // 30 minutes
 
-    const ws = new WebSocketServer({ port: 1234 });
-
-    ws.on('connection', function connection(ws: any) {
-        ws.on('message', function incoming() {
-            ws.send(token);
-        });
+    const wss = new WebSocketServer({ port: 1234 });
+    wss.on('connection', function connection(ws: any) {
+        log('New client connected');
+        client = ws;
     });
+
+    setInterval(async () => {
+        if (client) {
+            const inrixData = await fetchInrixData();
+            log(`Pushing ${inrixData.length} segments to client`);
+            if (inrixData) {
+                inrixData.forEach((data: any) => sendJsonToClient(data));
+            }
+            log(`Done`);
+        }
+    }, 2 * 60000); // 2 minutes
+};
+
+const sendJsonToClient = (obj: any) => {
+    client.send(JSON.stringify(obj));
 };
 
 // Done this way so that it gets around top level await
